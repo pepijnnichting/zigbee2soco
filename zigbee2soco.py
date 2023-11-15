6f99d54e75783318d1fc00c3c2fc164334bf0a12
Pepijn Nichting <pepijn.nichting@gmail.com>
2023-11-15 22:27:22 +0100

    nu werkend met de oude ikea draai remote

--- zigbee2soco.py
+++ zigbee2soco.py
@@ -2,11 +2,19 @@
 
 import sys 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # debug code in case docker doesn't find the modules
 #for path in sys.path:
 #    print(path)
 
+
+try:
+    socozone=int(os.environ.get("SONOS_ZONE"))
+except:
+    socozone="Woonkamer Sonos"
 
 try:
     multiplier=int(os.environ.get("VOLUME_MULTIPLIER"))
@@ -16,18 +24,17 @@ except:
 try:
     mqttprefix=os.environ.get("PREFIX")
 except:
-    mqttprefix="zigbee/stereo"
+    mqttprefix="zigbee2mqtt/sonosremote"
 
 try:
     mqtthost=os.environ.get("MQTT_HOST")
 except:
-    mqtthost="localhost"
+    mqtthost="lcalhost"
 
 try:
     mqttport=os.environ.get("MQTT_PORT")
 except:
     mqttport=1883
-
 
 try:
     mqttuser=os.environ.get("MQTT_USER")
@@ -43,6 +50,7 @@ except:
 import paho.mqtt.client as mqtt
 import soco
 import traceback
+import json
 
 
 
@@ -54,14 +62,20 @@ class Z2S:
         self.discover()
         
     def discover(self):
-        self.zones = {x.player_name:x for x in  soco.discover()}
+        discovered = soco.discover()
+        if discovered is not None:
+            self.zones = {x.player_name: x for x in discovered}
+            print("ZONES: "+str(self.zones))
+        else:
+            self.zones = {}
+            print("No zones discovered")
+
         
-        print("ZONES: "+str(self.zones))
         return self.zones
 
     def pause(self, speaker):
         self.state = self.zones[speaker].get_current_transport_info()['current_transport_state']
-        #priant(state)
+        #print(state)
 
         if self.state == "PLAYING":
             print("Pause "+speaker)
@@ -106,57 +120,43 @@ def on_connect(client, z2s, flags, rc):
         
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(mqttprefix+"/+/action")
+    
+    print("trying to subscribe to "+mqttprefix)
+    client.subscribe(mqttprefix)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, z2s, msg):
 
-    print(msg.topic+" "+str(msg.payload))
-    
-    payload = msg.payload.decode("utf-8")
-
-    try:
-        topic = msg.topic
-        topic = topic.replace(mqttprefix+"/","")
-        topic = topic.replace("/action","")
-        #print(topic)
-        #print(zones)
-
-    except:
-        print(traceback.format_exc())
-        print (sys.exc_info()[0])
+    decoded_message=str(msg.payload.decode("utf-8"))
+    payload=json.loads(decoded_message)
+    action = payload.get('action')
+    volume = payload.get('brightness')
+    # print(decoded_message)
+    print(action)
+    print(volume)
 
 
     # move this to the object
-    if not topic in z2s.zones:
-        print("No such speaker "+topic+" running discover")
+    if not socozone in z2s.zones:
+        print("No such speaker "+socozone+" running discover")
         z2s.discover()
 
-        if not topic in z2s.zones:
+        if not socozone in z2s.zones:
             print ("Not found after rescan")
             return
 
-    if payload == "play_pause":
+    if action == "play_pause" or action == "toggle":
         # both gen1 and gen2 have play_pause
-        z2s.pause(topic)
-    elif payload == "skip_forward" or payload == "track_next":
+        z2s.pause(socozone)
+    elif action == "skip_forward" or action == "track_next":
         # gen1 - skip_forward, gen2 - track_next
-        z2s.skipforward(topic)
-    elif payload == "rotate_right" or payload == "volume_up" or payload == "volume_up_hold":
+        z2s.skipforward(socozone)
+    elif action == "rotate_right" or action == "volume_up" or action == "brightness_move_up":
         # gen1 - rotate, gen2 - volume...
-        z2s.volup(topic)
-    elif payload == "rotate_left"  or payload == "volume_down" or payload == "volume_down_hold": 
+        z2s.volup(socozone)
+    elif action == "rotate_left"  or action == "volume_down" or action == "brightness_move_down": 
         # gen1 - rotate, gen2 - volume...
-        z2s.voldown(topic)
-
-    # not implemented:
-    # dots buttons
-    
-    # skip_backward
-    # skip_backward can be implemented by calling device_previous() but (in my experience) the wanted behavior is
-    # to reset the currently playing tune to 0 at the first click, then, if the skip_backward is pressed again before
-    # (a short time) has elapsed, we jump back one tune. This means that we need to get the play time, check it, then do something
-    
+        z2s.voldown(socozone)
         
 ################################
 
@@ -164,7 +164,7 @@ z2s = Z2S()
     
 client = mqtt.Client(userdata=z2s)
 if mqttuser:
-    #print ("Using mqtt user name "+mqttuser+" / password '"+mqttpass+"'")
+    print ("Using mqtt user name "+mqttuser+" / password '"+mqttpass+"'")
     client.username_pw_set(mqttuser, mqttpass)
 client.on_connect = on_connect
 client.on_message = on_message
