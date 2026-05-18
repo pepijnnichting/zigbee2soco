4a4c8c3775f8a1c7381fef68a8828b404d04975b
Pepijn Nichting <pepijn.nichting@gmail.com>
2026-05-18 15:34:07 +0200

    refactor: streamline environment variable loading and improve logging

--- zigbee2soco.py
+++ zigbee2soco.py
@@ -1,56 +1,20 @@
 #!/usr/bin/python3
 
-import sys 
 import os
+import json
+import paho.mqtt.client as mqtt
+import soco
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# debug code in case docker doesn't find the modules
-#for path in sys.path:
-#    print(path)
-
-
-try:
-    socozone=int(os.environ.get("SONOS_ZONE"))
-except:
-    socozone="Woonkamer Sonos"
-
-try:
-    multiplier=int(os.environ.get("VOLUME_MULTIPLIER"))
-except:
-    multiplier=1
-
-try:
-    mqttprefix=os.environ.get("MQTT_PREFIX")
-except:
-    mqttprefix="zigbee2mqtt/sonosremote"
-
-try:
-    mqtthost=os.environ.get("MQTT_HOST")
-except:
-    mqtthost="localhost"
-
-try:
-    mqttport=os.environ.get("MQTT_PORT")
-except:
-    mqttport=1883
-
-try:
-    mqttuser=os.environ.get("MQTT_USER")
-except:
-    mqttuser=None
-    
-try:
-    mqttpass=os.environ.get("MQTT_PASS")
-except:
-    mqttpass=None
-
-    
-import paho.mqtt.client as mqtt
-import soco
-import traceback
-import json
+socozone  = os.getenv("SONOS_ZONE", "Woonkamer Sonos")
+multiplier = int(os.getenv("VOLUME_MULTIPLIER", "1"))
+mqttprefix = os.getenv("MQTT_PREFIX", "zigbee2mqtt/sonosremote")
+mqtthost   = os.getenv("MQTT_HOST", "localhost")
+mqttport   = int(os.getenv("MQTT_PORT", "1883"))
+mqttuser   = os.getenv("MQTT_USER")
+mqttpass   = os.getenv("MQTT_PASS")
 
 
 
@@ -66,48 +30,41 @@ class Z2S:
         discovered = soco.discover()
         if discovered is not None:
             self.zones = {x.player_name: x for x in discovered}
-            print("ZONES: "+str(self.zones))
+            print(f"ZONES: {self.zones}")
         else:
             self.zones = {}
             print("No zones discovered")
 
-        
         return self.zones
 
     def pause(self, speaker):
-        self.state = self.zones[speaker].get_current_transport_info()['current_transport_state']
-        #print(state)
+        state = self.zones[speaker].get_current_transport_info()['current_transport_state']
 
-        if self.state == "PLAYING":
-            print("Pause " + speaker)
+        if state == "PLAYING":
+            print(f"Pause {speaker}")
             self.zones[speaker].pause()
-        
         else:
-            print("Play " + speaker)
-            try:                
+            print(f"Play {speaker}")
+            try:
                 self.zones[speaker].play()
             except Exception as e:
                 print(f"Unable to play tune on {speaker}. Try playing something from the Sonos controller first. Error: {e}")
-                pass
 
     def skipforward(self, speaker):
-        print("skip forward "+speaker)
-
+        print(f"skip forward {speaker}")
         self.zones[speaker].next()
 
     def volup(self, speaker):
-        self.state = self.zones[speaker].get_current_transport_info()['current_transport_state']
-        if self.state == "PLAYING":
-            print("volume up " + speaker)
-            nv = min(self.zones[speaker].volume + multiplier, 100)
-            self.zones[speaker].volume = nv
+        state = self.zones[speaker].get_current_transport_info()['current_transport_state']
+        if state == "PLAYING":
+            print(f"volume up {speaker}")
+            self.zones[speaker].volume = min(self.zones[speaker].volume + multiplier, 100)
 
     def voldown(self, speaker):
-        self.state = self.zones[speaker].get_current_transport_info()['current_transport_state']
-        if self.state == "PLAYING":
-            print("volume down " + speaker)
-            nv = max(self.zones[speaker].volume-multiplier,0)
-            self.zones[speaker].volume = nv
+        state = self.zones[speaker].get_current_transport_info()['current_transport_state']
+        if state == "PLAYING":
+            print(f"volume down {speaker}")
+            self.zones[speaker].volume = max(self.zones[speaker].volume - multiplier, 0)
 
         
 
@@ -115,55 +72,49 @@ class Z2S:
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reasonCode, properties=None):
-    print("MQTT Connected with result code "+str(reasonCode))
+    print(f"MQTT Connected with result code {reasonCode}")
     if reasonCode == 4:
         print("MQTT connection refused - bad username or password")
     elif reasonCode == 5:
         print("MQTT connection refused - not authorized")
-        
+
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    
-    print("trying to subscribe to "+mqttprefix)
+
+    print(f"trying to subscribe to {mqttprefix}")
     client.subscribe(mqttprefix)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-
-    decoded_message=str(msg.payload.decode("utf-8"))
-    payload=json.loads(decoded_message)
+    payload = json.loads(msg.payload.decode("utf-8"))
     action = payload.get('action')
-    volume = payload.get('brightness')
-    # print(decoded_message)
-    # print(action)
-    # print(volume)
 
     if action == "brightness_move_up":
         z2s.lastUporDown = "up"
     elif action == "brightness_move_down":
-        z2s.lastUporDown = "down"   
+        z2s.lastUporDown = "down"
     elif action == "brightness_stop":
         z2s.lastUporDown = None
 
     # move this to the object
-    if not socozone in z2s.zones:
-        print("No such speaker "+socozone+" running discover")
+    if socozone not in z2s.zones:
+        print(f"No such speaker {socozone}, running discover")
         z2s.discover()
 
-        if not socozone in z2s.zones:
-            print ("Not found after rescan")
+        if socozone not in z2s.zones:
+            print("Not found after rescan")
             return
 
-    if action == "play_pause" or action == "toggle":
+    if action in ("play_pause", "toggle"):
         # both gen1 and gen2 have play_pause
         z2s.pause(socozone)
-    elif action == "skip_forward" or action == "track_next":
+    elif action in ("skip_forward", "track_next"):
         # gen1 - skip_forward, gen2 - track_next
         z2s.skipforward(socozone)
-    elif action == "rotate_right" or action == "volume_up" or action == "brightness_move_up" or (action == None and z2s.lastUporDown == "up"):
+    elif action in ("rotate_right", "volume_up", "brightness_move_up") or (action is None and z2s.lastUporDown == "up"):
         # gen1 - rotate, gen2 - volume...
         z2s.volup(socozone)
-    elif action == "rotate_left"  or action == "volume_down" or action == "brightness_move_down" or (action == None and z2s.lastUporDown == "down"):
+    elif action in ("rotate_left", "volume_down", "brightness_move_down") or (action is None and z2s.lastUporDown == "down"):
         # gen1 - rotate, gen2 - volume...
         z2s.voldown(socozone)
         
@@ -173,15 +124,15 @@ z2s = Z2S()
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.user_data_set(z2s)
 if mqttuser:
-    print ("Using mqtt user name "+mqttuser+" / password '"+mqttpass+"'")
+    print(f"Using mqtt user name {mqttuser} / password '{mqttpass}'")
     client.username_pw_set(mqttuser, mqttpass)
 client.on_connect = on_connect
 client.on_message = on_message
 
-print ("Connecting to "+mqtthost+":"+str(mqttport))
-client.connect(mqtthost, int(mqttport), 60)
+print(f"Connecting to {mqtthost}:{mqttport}")
+client.connect(mqtthost, mqttport, 60)
 
-print ("zigbee2soco starting processing of events")
+print("zigbee2soco starting processing of events")
 
 # mqtt loop
 client.loop_forever()
